89a788b8ce483a93f9085e6b05d8c8e42f257cc0
hetsketch <al3xander.koval@gmail.com>
2016-06-21 23:28:52 +0300

    Test for PhoneBook have been written

--- src/test/java/phonebook/PhoneBookTest.java
+++ src/test/java/phonebook/PhoneBookTest.java
@@ -1,7 +1,45 @@
 package phonebook;
 
-/**
- * Created by hetsketch on 6/21/16.
- */
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class PhoneBookTest {
+    private static PhoneBook phoneBook;
+
+    public PhoneBookTest() {
+    }
+
+    @BeforeClass
+    public static void initializePhoneBookTest() {
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void testGetPhonesWithProperLastName() {
+        List<String> phones = phoneBook.getPhones("Иванов И.И.");
+
+        assertNotNull(phones);
+        assertTrue(phones.contains("+8 800 2000 500"));
+        assertTrue(phones.contains("+8 800 200 600"));
+    }
+
+    @Test
+    public void testGetPhonesWithNotExistingLastName()  {
+        List<String> phones = phoneBook.getPhones("Some last name");
+
+        assertNotNull(phones);
+        assertTrue(phones.size() == 0);
+    }
+
+    @Test()
+    public void testGetPhonesWithNull()  {
+        List<String> phones = phoneBook.getPhones(null);
+        assertNotNull(phones);
+        assertTrue(phones.size() == 0);
+    }
 }
